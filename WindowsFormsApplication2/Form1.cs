455e74945eb933bfaa9fa4a4bc2f086d4389e144
thomasxd24 <tomas_tse24@me.com>
2015-03-06 19:48:37 +0100

    added excel export

--- WindowsFormsApplication2/Form1.cs
+++ WindowsFormsApplication2/Form1.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
 
+
 namespace WindowsFormsApplication2
 {
     public partial class Form1 : Form
--- WindowsFormsApplication2/Form2.cs
+++ WindowsFormsApplication2/Form2.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using ExcelLibrary;
 namespace WindowsFormsApplication2
 {
     public partial class Form2 : Form
@@ -20,7 +20,36 @@ namespace WindowsFormsApplication2
 
         private void button1_Click(object sender, EventArgs e)
         {
+            DataSet ds = new DataSet("final");
+            ds.Tables.Add(DataGridView2DataTable(dataGridView1, "final"));
+            ExcelLibrary.DataSetHelper.CreateWorkbook("Le bon de commande.xls", ds);
+        }
 
+        public DataTable DataGridView2DataTable(DataGridView dgv, String tblName)
+        {
+
+             DataTable dt = new DataTable(tblName);
+
+            // Header columns
+            foreach (DataGridViewColumn column in dgv.Columns)
+            {
+                DataColumn dc = new DataColumn(column.Name.ToString());
+                dt.Columns.Add(dc);
+            }
+
+            // Data cells
+            for (int i = 0; i < dgv.Rows.Count; i++)
+            {
+                DataGridViewRow row = dgv.Rows[i];
+                DataRow dr = dt.NewRow();
+                for (int j = 0; j < dgv.Columns.Count; j++)
+                {
+                    dr[j] = (row.Cells[j].Value == null) ? "" : row.Cells[j].Value.ToString();
+                }
+                dt.Rows.Add(dr);
+            }
+
+            return dt;
         }
 
     }
